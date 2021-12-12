5ff84bee44cb8d8c17eba26ac7b93561205cd29e
Shelby Suits <srsuits@crimson.ua.edu>
2021-12-12 15:19:20 -0500

    Vector input functionality working

--- vector_math.py
+++ vector_math.py
@@ -8,30 +8,41 @@ Created on Sun Dec 12 13:59:06 2021
 import numpy as np
 
 
-vec1 = np.array([1,1,1])
+#vec1 = np.array([1,1,1])
 
-vec2 = np.array([1,1,1])
+#vec2 = np.array([1,1,1])
 
 
-
-def get_vectors(num_vectors, size_vectors):
+# Get vector(s) from user input
+def get_vectors(num_v, size_v):
     
+    # Create list to hold return vectors
+    vectors = []
     
-    for vec in range(0, num_vectors):
+    # For each vector 
+    for vec in range(0, num_v):
         
+        # Create empty list for individual vector
         vector_list = []
-
-        for i in range(0,size_vectors):
+        
+        # For each element in vector
+        for i in range(0,size_v):
             
+            # Get the element 
             element = input("enter value number " + str(i + 1) + " for vector " + str(vec + 1) + ": ")
             
+            # Check that input is numerical
             if element.isdigit():
+                
+                # Add new element to vector
                 vector_list.append(float(element))
             else:
                 print("please enter a numerical value")
             
-
-            print(element)
+        # Add finish vector to vectors 
+        vectors.append(vector_list)
+        
+    return(vectors)
             
             
     
@@ -52,24 +63,72 @@ def do_math(text):
     # Ignore all capitalization
     text = text.lower()
     
+    vec_size = input("Enter the size(dimension) of your vector(s): ")
+    
+    try:
+        vec_size = int(vec_size)
+    except:
+        print("ERROR: please enter an integer value")
+    
+    
     if text == 'addition':
+        # Get vectors from user input
+        vectors = get_vectors(2, vec_size)
+
+        # Separate vector lists and cast to np array
+        vec1 = np.array(vectors[0])
+        vec2 = np.array(vectors[1])
         
-        vectors = get_vectors(2, 3)
-        
+        # Math
         new_vec = vec1 + vec2
         
-        
     elif text == 'subtraction':
+        # Get vectors from user input
+        vectors = get_vectors(2, vec_size)
+
+        # Separate vector lists and cast to np array
+        vec1 = np.array(vectors[0])
+        vec2 = np.array(vectors[1])
+        
+        # Math
         new_vec = vec1 - vec2
+
     elif text == 'multiplication':
+        # Get vectors from user input
+        vectors = get_vectors(2, vec_size)
+
+        # Separate vector lists and cast to np array
+        vec1 = np.array(vectors[0])
+        vec2 = np.array(vectors[1])
+        
+        # Math        
         new_vec = vec1 * vec2
+        
     elif text == 'scalar multiplication':
+        # Get vector and scalar from user input
         scalar = float(input("Enter the scalar to multiply your vector by: "))
+        vec1 = np.array(get_vectors(1, vec_size)[0])
+        
+        # Math
         new_vec = vec1 * scalar
+        
     elif text == 'dot product':
+        # Get vectors from user input
+        vectors = get_vectors(2, vec_size)
+
+        # Separate vector lists and cast to np array
+        vec1 = np.array(vectors[0])
+        vec2 = np.array(vectors[1])
+        
+        # Math
         new_vec = np.dot(vec1,vec2)
+        
     elif text == 'constant addition':
+        # Get vector and constant from user input
         constant = float(input("Enter the constant to add to your vector: "))
+        vec1 = np.array(get_vectors(1, vec_size)[0])
+        
+        # Math
         new_vec = vec1 + constant
     else:
         print("\nERROR: incorrect input \nEnter only letters \nChoose one of the options listed\n")
